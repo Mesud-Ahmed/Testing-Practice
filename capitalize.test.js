ed085320e6cf328610cd4d2b10d1343c3fde29e5
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-09-16 12:02:40 +0300

    feat: implement capitalize function

--- capitalize.test.js
+++ capitalize.test.js
@@ -5,5 +5,20 @@ describe('capitalize function', () => {
     test(" 'car' becomes 'Car'", () => {
         expect(capitalize("car")).toBe("Car")
     })
+    test("capitalizes the first character of a sentence", () => {
+        expect(capitalize("this is a test")).toBe("This is a test")
+    })
     
+    test('handles single charcter strings',()=>{
+        expect(capitalize('a')).toBe("A")
+    })
+    test('returns empty string for empty input',()=>{
+        expect(capitalize("")).toBe("")
+    })
+    test("ignores non alphabetic first character",()=>{
+        expect(capitalize("12ab")).toBe("12ab")
+    })
+    test("doesn't change the rest of the string",()=>{
+        expect(capitalize('heLLO')).toBe("HeLLO")
+    })
 })

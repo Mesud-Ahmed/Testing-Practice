f87efa05d853a9b12e7f6a30fa018ac0dbf278c7
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-09-16 17:49:19 +0300

    test: add tests for caesarCipher function

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,24 +1,24 @@
 export function caesarCipher(str, factor) {
-    factor = factor % 26; // Ensure the factor is within 0-25
+    factor = factor % 26; 
     let shifted = "";
 
     for (let i = 0; i < str.length; i++) {
         let char = str[i];
 
-        if (char.match(/[a-z]/i)) { // Check if the character is an alphabet
+        if (char.match(/[a-z]/i)) { 
             let code = str.charCodeAt(i);
 
-            // Uppercase letters
+
             if (code >= 65 && code <= 90) {
                 char = String.fromCharCode(((code - 65 + factor) % 26) + 65);
             }
-            // Lowercase letters
+           
             else if (code >= 97 && code <= 122) {
                 char = String.fromCharCode(((code - 97 + factor) % 26) + 97);
             }
         }
 
-        // Append either the shifted or original character
+       
         shifted += char;
     }
 
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,7 +1,13 @@
 import { caesarCipher } from "./caesarCipher"
 
 describe("caesarCipher function", () => {
-test("test wrapping from z to a",()=>{
-    expect(caesarCipher('xyz',3)).toBe('abc')
-})
+    test("test wrapping from z to a", () => {
+        expect(caesarCipher('xyz', 3)).toBe('abc')
+    })
+    test("handle test case preservation", () => {
+        expect(caesarCipher('HeLLo', 3)).toBe('KhOOr')
+    })
+    test("Punctuations, spaces, and other non-alphabetical characters should remain unchanged", () => {
+        expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
+    })
 })

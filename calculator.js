7244ad217e41e4f999f756f2ac992d10e04f6b4c
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-09-16 17:14:24 +0300

    feat: implement calculator Object

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
-const calculator = {
+export const calculator = {
     add: (a, b) => a + b,
     subtract: (a, b) => a - b,
-    multiply: (a, b) => (a * b).toFixed(2),
-    divide: (a, b) => (b === 0 && a !== 0) ? Infinity : (a / b).toFixed(2), 
+    multiply: (a, b) => parseFloat((a * b).toFixed(2)),
+    divide: (a, b) => (b === 0 && a !== 0) ? Infinity : parseFloat((a / b).toFixed(2))
 };
--- calculator.test.js
+++ calculator.test.js
@@ -1,3 +1,5 @@
+import { calculator } from "./calculator"
+
 describe("calculator Object", () => {
     test("handles 2 positive numbers", () => {
         expect(calculator.add(3, 2)).toBe(5)
@@ -8,29 +10,29 @@ describe("calculator Object", () => {
     })
 
     test("handles 2 positive numbers", () => {
-        expect(calculator.divide(4, 2)).toBe(2)
+        expect(calculator.divide(4, 2)).toBe(2.00)
     })
 
     test("handles division by zero", () => {
         expect(calculator.divide(4, 0)).toBe(Infinity)
     })
     test("handles floating results", () => {
-        expect(calculator.divide(3, 2)).toBe(1.5)
+        expect(calculator.divide(3, 2)).toBe(1.50)
     })
     test("rounds to two decimal places for non terminating results", () => {
         expect(calculator.divide(10, 3)).toBe(3.33)
     })
     test("handles 2 positive numbers", () => {
-        expect(calculator.multiply(3, 2)).toBe(6)
+        expect(calculator.multiply(3, 2)).toBe(6.00)
     })
 
     test("rounds to two decimal places for fractional numbers ", () => {
-        expect(calculator.multiply(3, 2)).toBe(6)
+        expect(calculator.multiply(3.3, 2.45)).toBe(8.09)
     })
     test('handles negative numbers', () => {
         expect(calculator.add(-5, -3)).toBe(-8);
         expect(calculator.subtract(-5, -3)).toBe(-2);
-        expect(calculator.multiply(-5, -3)).toBe(15);
-        expect(calculator.divide(-6, -3)).toBe(2);
+        expect(calculator.multiply(-5, -3)).toBe(15.00);
+        expect(calculator.divide(-6, -3)).toBe(2.00);
     });
 })
